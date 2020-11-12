3fb9a5a0f69e5f568f575fda47ea03758ea6b0d0
Isuru Dissanayake <isurumailme@gmail.com>
2020-11-12 21:57:43 +0530

    Changed assembly code

--- AssemblyGenerator.py
+++ AssemblyGenerator.py
@@ -7,49 +7,51 @@ Assembly= {0: 'RST ALL',
 6: 'MUL R1 R2',
 7: 'ROOF AC R3',
 8: 'MOV AC R14',
-9: 'MUL R13 I',
-10: 'MOV AC R7',
-11: 'MUL R1 R2',
-12: 'SUB AC R7',
-13: 'MUL R13 R3',
-14: 'ADD AC I',
-15: 'MOV AC R4',
-16: 'SUBONE AC',
-17: 'MOD AC R1',
-18: 'MOV AC R5',
-19: 'FLR R4 R1',
-20: 'MOV AC R4',
-21: 'MUL R6 R4',
-22: 'LOAD 4 R7',
-23: 'LOAD 5 R8',
-24: 'LOAD 6 R9',
-25: 'ADD AC R7',
-26: 'MOV AC R7',
-27: 'ADD R8 R5',
-28: 'MOV AC R8',
-29: 'MUL R1 R4',
-30: 'ADD AC R5',
-31: 'ADD AC R9',
-32: 'MOV AC R9',
-33: 'LOADR R7 R10',
-34: 'LOADR R8 R11',
-35: 'MUL R10 R11',
-36: 'ADD AC TO',
-37: 'MOV AC TO',
-38: 'ADDONE R7',
-39: 'MOV AC R7',
-40: 'ADD R8 R1',
-41: 'MOV AC R8',
-42: 'ADDONE R12',
-43: 'MOV AC R12',
-44: 'SUB R6 R12',
-45: 'JUMPNZ 34',
-46: 'STORE R9 TO',
-47: 'RST TO',
-48: 'ADDONE R13',
-49: 'MOV AC R13',
-50: 'SUB R14 R13',
-51: 'JUMPNZ 10'}
+9: 'ADDONE R13',
+10: 'MOV AC R13',
+11: 'MUL R13 I',
+12: 'MOV AC R7',
+13: 'MUL R1 R2',
+14: 'SUB AC R7',
+15: 'MUL R13 R3',
+16: 'ADD AC I',
+17: 'MOV AC R4',
+18: 'SUBONE AC',
+19: 'MOD AC R1',
+20: 'MOV AC R5',
+21: 'FLR R4 R1',
+22: 'MOV AC R4',
+23: 'MUL R6 R4',
+24: 'LOAD 4 R7',
+25: 'LOAD 5 R8',
+26: 'LOAD 6 R9',
+27: 'ADD AC R7',
+28: 'MOV AC R7',
+29: 'ADD R8 R5',
+30: 'MOV AC R8',
+31: 'MUL R1 R4',
+32: 'ADD AC R5',
+33: 'ADD AC R9',
+34: 'MOV AC R9',
+35: 'LOADR R7 R10',
+36: 'LOADR R8 R11',
+37: 'MUL R10 R11',
+38: 'ADD AC TO',
+39: 'MOV AC TO',
+40: 'ADDONE R7',
+41: 'MOV AC R7',
+42: 'ADD R8 R1',
+43: 'MOV AC R8',
+44: 'ADDONE R12',
+45: 'MOV AC R12',
+46: 'SUB R6 R12',
+47: 'JUMPNZ 36',
+48: 'STORE R9 TO',
+49: 'RST TO',
+50: 'ADDONE R13',
+51: 'MOV AC R13',
+52: 'SUB R14 R13',
+53: 'JMPNZ 12'}
 
 
 #size of the first matrix= n*m
@@ -73,7 +75,7 @@ B= [[1,2],
     [3,4]]
 
 f= open(str(n)+'x'+str(l)+'.txt',"w+")
-for i in range(52):
+for i in range(54):
     #if i== 22:
     #    f.write(str(i)+' LOADR '+str(alpha1)+' R7\n')
     #elif i== 23:
@@ -82,7 +84,8 @@ for i in range(52):
     #    f.write(str(i)+' LDI '+str(alpha3)+' R9\n')
     #else:
         
-    f.write(str(i)+' '+Assembly[i]+'\n')
+    #f.write(str(i)+' '+Assembly[i]+'\n')
+    f.write(Assembly[i]+'\n')
 
 f.close() 
 
